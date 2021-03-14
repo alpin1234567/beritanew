60fa958641e1c93502e84749bf55348e434198a3
alpin1234567 <74572180+alpin1234567@users.noreply.github.com>
2021-03-14 15:51:25 +0700

    Add files via upload

---
+++ api/controllers/index.ts
@@ -0,0 +1,23 @@
+import CnnNews from './newshandler/CnnNews'
+import CnbcNews from './newshandler/CnbcNews'
+import RepublikaNews from './newshandler/RepublikaNews'
+import TempoNews from './newshandler/TempoNews'
+import AntaraNews from './newshandler/AntaraNews'
+import KumparanNews from './newshandler/KumparanNews'
+import OkezoneNews from './newshandler/OkezoneNews'
+import Liputan6News from './newshandler/Liputan6News'
+import BbcNews from './newshandler/BbcNews'
+import notFound from './404'
+
+export default {
+    CnnNews,
+    CnbcNews,
+    RepublikaNews,
+    TempoNews,
+    AntaraNews,
+    KumparanNews,
+    OkezoneNews,
+    Liputan6News,
+    BbcNews,
+    notFound
+}
